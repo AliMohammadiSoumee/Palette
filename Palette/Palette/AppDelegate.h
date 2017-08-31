f853f9a4b693db97085a70ab1a75b7ceaf4e0d7b
Ali Soume'e <alidadar7676@gmail.com>
2017-08-31 14:43:25 +0430

    Add the Xcode project.

---
+++ Palette/Palette/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Palette
+//
+//  Created by Ali Soume`e on 6/9/1396 AP.
+//  Copyright © 1396 Ali Soume`e. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Palette/Palette/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Palette
+//
+//  Created by Ali Soume`e on 6/9/1396 AP.
+//  Copyright © 1396 Ali Soume`e. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
